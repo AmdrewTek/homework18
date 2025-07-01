abb3621f01e1d276689896f69a1157ef34b65f45
AndrewTek <tekunov.andrei@gmail.com>
2025-07-01 20:20:13 +0300

    Правки запуска локально и в Jenkins 2

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,13 +1,13 @@
 package tests;
 
 public class TestData {
-  public static final String USERNAME = System.getProperty("userName", System.getenv("USER_NAME"));
-  public static final String PASSWORD = System.getProperty("password", System.getenv("USER_PASSWORD"));
+  public static final String USERNAME = System.getProperty("USERNAME", System.getenv("USER_NAME"));
+  public static final String PASSWORD = System.getProperty("PASSWORD", System.getenv("USER_PASSWORD"));
   public static final String isbn = "9781491904244";
 
   static {
     if (USERNAME == null || USERNAME.isBlank() || PASSWORD == null || PASSWORD.isBlank()) {
-      throw new IllegalStateException("Username and password must be provided via -DuserName/-Dpassword or environment variables USER_NAME/USER_PASSWORD");
+      throw new IllegalStateException("Username and password must be provided via -DUSERNAME/-DPASSWORD or environment variables USER_NAME/USER_PASSWORD");
     }
   }
 }
